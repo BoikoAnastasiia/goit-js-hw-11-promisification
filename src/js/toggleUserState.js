e8291328ee999808e651bd76f08edf4d63f6f5af
anastasyah86 <nastasyah86@gmail.com>
2021-01-29 16:25:28 +0200

    2 have done

---
+++ src/js/toggleUserState.js
@@ -0,0 +1,31 @@
+// Перепиши функцию toggleUserState() так,
+//     тобы она не использовала callback - функцию callback,
+//     a   принимала всего два параметра allUsers
+// и userName и возвращала промис.
+
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (userName, allUsers) => {
+  return new Promise(resolve => {
+    resolve({ userName, allUsers });
+  });
+};
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+/*
+ * Сейчас работает так
+ */
+// toggleUserState(users, 'Mango', logger);
+// toggleUserState(users, 'Lux', logger);
+
+/*
+ * Должно работать так
+ */
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
